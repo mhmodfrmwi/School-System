2fb4991e15804e88373b42f8ea4462c1233c967e
mhmodfrmwi <mahmoudframawi98@gmail.com>
2024-11-03 10:20:00 +0200

    test controllers

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { Login, Register, hello } = require("../controllers/authController");
 const router = express.Router();
 
-router.post("/login", Login);
+router.get("/login", Login);
 router.post("/register", Register);
 module.exports = router;
