ac4ba99171e61da0a508e933b942938b2bea6048
WessamMohamed22 <wm8821730@gmail.com>
2025-01-21 22:17:58 +0200

    Update

--- frontend/src/ui/PageNotFound.jsx
+++ frontend/src/ui/PageNotFound.jsx
@@ -1,9 +1,11 @@
 import NotFound from '../assets/notfound2.png';
+
 export default function PageNotFound() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-      <img src={NotFound} alt="Page Not Found" />
-      <h1 style={{ marginTop: "20px", fontSize: "24px", fontFamily: "'Poppins', sans-serif", color: "#117C90", textAlign: "center" }}>
+    <div className="flex flex-col items-center justify-center">
+      <img src={NotFound} alt="Page Not Found" className="w-[500px] h-auto" // Adjust the width as needed
+ />
+      <h1 className="mt-5 text-2xl font-poppins text-[#117C90] text-center">
         Page Not Found
       </h1>
     </div>
