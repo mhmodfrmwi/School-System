f15dd37736e18709d0adced6edadf49cfdff00ab
mhmodfrmwi <mahmoudframawi98@gmail.com>
2025-01-27 20:23:52 +0200

    adding profile image

--- backend/DB/student.js
+++ backend/DB/student.js
@@ -25,8 +25,14 @@ const studentSchema = new mongoose.Schema({
     ref: "AcademicYear",
     required: true,
   },
+  profileImage: {
+    type: String,
+    default: path.join(__dirname, "../images/student.png"),
+    default:
+      "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1733078248~exp=1733081848~hmac=e2eec059dc2217f905388f452eaf9dee7d9679765c2b0ac7f273a5cfe7cd4bde&w=740",
+  },
 });
-studentSchema.methods.comparePasswordInDb = async function(pswd, pswdDB){
+studentSchema.methods.comparePasswordInDb = async function (pswd, pswdDB) {
   return await bcrypt.compare(pswd, pswdDB);
-}
+};
 module.exports = mongoose.model("Student", studentSchema);
--- backend/DB/teacher.js
+++ backend/DB/teacher.js
@@ -14,6 +14,12 @@ const teacherSchema = new mongoose.Schema({
     ref: "Subject",
     required: true,
   },
+  profileImage: {
+    type: String,
+    default: path.join(__dirname, "../images/student.png"),
+    default:
+      "https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?t=st=1733078248~exp=1733081848~hmac=e2eec059dc2217f905388f452eaf9dee7d9679765c2b0ac7f273a5cfe7cd4bde&w=740",
+  },
 });
 const Teacher = mongoose.model("Teacher", teacherSchema);
 module.exports = Teacher;
