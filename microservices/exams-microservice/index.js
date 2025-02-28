84d66b9471582e1330f32d434b02aa99c00ab830
mhmodfrmwi <mahmoudframawi98@gmail.com>
2025-02-28 17:53:55 +0200

    implementation of getting all exams and subject exams

--- microservices/exams-microservice/index.js
+++ microservices/exams-microservice/index.js
@@ -48,3 +48,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Exams microservice running on port ${PORT}`);
 });
+
+/*
+  [ ] fetch exams with gradeSubjectSemester
+  [ ] teacher get, update , delete  
+*/
--- microservices/exams-microservice/services/examService.js
+++ microservices/exams-microservice/services/examService.js
@@ -1,24 +1,63 @@
 const Exam = require("../models/Exam");
 
 const addExam = async (examData) => {
-  const exam = new Exam(examData);
-  // await exam.save();
-  return exam;
+  try {
+    console.log(examData);
+    const exam = new Exam(examData);
+    await exam.save();
+    return exam;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
 };
 const fetchExams = async () => {
-  const exams = await Exam.find()
-    .populate(
-      "subject_id grade_id class_id academic_year_id semester_id created_by"
-    )
-    .select("-__v -createdAt -updatedAt");
-  return exams;
+  try {
+    const exams = await Exam.find()
+      .populate("subject_id grade_id class_id academic_year_id semester_id")
+      .populate("created_by", "_id fullName")
+      .select("-__v -createdAt -updatedAt");
+    return exams;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
+};
+const fetchExamsByAttributes = async (
+  class_id,
+  semester_id,
+  grade_id,
+  subject_id,
+  academic_year_id
+) => {
+  try {
+    const exams = await Exam.find({
+      class_id,
+      semester_id,
+      grade_id,
+      subject_id,
+      academic_year_id,
+    })
+      .populate("subject_id grade_id class_id academic_year_id semester_id")
+      .populate("created_by", "_id fullName")
+      .select("-__v -createdAt -updatedAt");
+    return exams;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
 };
 const fetchExamById = async (id) => {
-  const exam = await Exam.findById(id)
-    .populate(
-      "subject_id grade_id class_id academic_year_id semester_id created_by"
-    )
-    .select("-__v -createdAt -updatedAt");
-  return exam;
+  try {
+    const exam = await Exam.findById(id)
+      .populate(
+        "subject_id grade_id class_id academic_year_id semester_id created_by"
+      )
+      .select("-__v -createdAt -updatedAt");
+    return exam;
+  } catch (error) {
+    console.error(error);
+    throw new Error(error.message);
+  }
 };
-module.exports = { addExam, fetchExams, fetchExamById };
+module.exports = { addExam, fetchExams, fetchExamById, fetchExamsByAttributes };
