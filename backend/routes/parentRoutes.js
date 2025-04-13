ebe8a05220eb1bbc18a28b1e796c73cd3e320b72
mhmodfrmwi <mahmoudframawi98@gmail.com>
2025-04-13 16:00:11 +0200

    feat: add parent student controllers for viewing and selecting kids
    - Add showKids to fetch all students associated with a parent
    - Add chooseKid to generate auth token for selected student

--- backend/routes/parentRoutes.js
+++ backend/routes/parentRoutes.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const { login } = require("../controllers/auth/authParentController");
+const {
+  showKids,
+  chooseKid,
+} = require("../controllers/Parent/parentController");
+const validateJwt = require("../middlewares/validateJWT");
+const validateParent = require("../middlewares/validateParent");
 const router = express.Router();
 router.post("/login", login);
 
+router.get("/kids", validateJwt, validateParent, showKids);
+router.post("/chooseKid", validateJwt, validateParent, chooseKid);
 module.exports = router;
